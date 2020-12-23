e21f51d8f19d1d3da68b08c1d1da1290651ab1b6
qin.xia <qin.xia@zhangmen.com>
2020-12-23 17:34:25 +0800

    add

---
+++ src/helloWorld.js
@@ -0,0 +1,13 @@
+// const a = 111111;
+export function helloworld() {
+  const b = 124;
+  const c = 12435;
+  if (b > c) {
+    return true;
+  }
+  return null;
+}
+
+export default {
+  helloworld,
+};
